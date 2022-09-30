4bc8c19108aa992e910a61f285c8c3f7a94bac33
CDTAdmin <cdtadmin@ndmpc898.ndm.ox.ac.uk>
2022-09-30 10:20:29 +0100

    Extracted the training loop from cli into ser.train

--- bin/cli.py
+++ bin/cli.py
@@ -2,10 +2,11 @@ from pathlib import Path
 import torch
 from torch import optim
 
-import torch.nn.functional as F
+
 import ser.model
 import ser.transforms 
 import ser.data
+import ser.train
 
 import typer
 
@@ -48,39 +49,7 @@ def train(
     training_dataloader, validation_dataloader = ser.data.get_dataloaders(batch_size, DATA_DIR, ts)
 
     # train
-    for epoch in range(epochs):
-        for i, (images, labels) in enumerate(training_dataloader):
-            images, labels = images.to(device), labels.to(device)
-            model.train()
-            optimizer.zero_grad()
-            output = model(images)
-            loss = F.nll_loss(output, labels)
-            loss.backward()
-            optimizer.step()
-            print(
-                f"Train Epoch: {epoch} | Batch: {i}/{len(training_dataloader)} "
-                f"| Loss: {loss.item():.4f}"
-            )
-            # validate
-            val_loss = 0
-            correct = 0
-            with torch.no_grad():
-                for images, labels in validation_dataloader:
-                    images, labels = images.to(device), labels.to(device)
-                    model.eval()
-                    output = model(images)
-                    val_loss += F.nll_loss(output, labels, reduction="sum").item()
-                    pred = output.argmax(dim=1, keepdim=True)
-                    correct += pred.eq(labels.view_as(pred)).sum().item()
-                val_loss /= len(validation_dataloader.dataset)
-                val_acc = correct / len(validation_dataloader.dataset)
-
-                print(
-                    f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}"
-                )
-
-
-
+    ser.train.train(model, epochs, training_dataloader, validation_dataloader, optimizer, device)
 
 @main.command()
 def infer():
