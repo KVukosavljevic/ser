262e285657a9edc79e8ad3273993ece5a2719907
CDTAdmin <cdtadmin@ndmpc898.ndm.ox.ac.uk>
2022-09-30 10:04:08 +0100

    Made all hyperparameters as inputs and separated the Model into ser.model

--- bin/cli.py
+++ bin/cli.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 import torch
 from torch import optim
-import torch.nn as nn
+
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+import ser.model
 
 import typer
 
@@ -19,17 +20,23 @@ def train(
     name: str = typer.Option(
         ..., "-n", "--name", help="Name of experiment to save under."
     ),
+    epochs: int = typer.Option(
+        ..., "-epochs", help="Number of epochs to run."
+    ),
+    batch_size: int = typer.Option(
+        ..., "-batch_size", help="Data batch size."
+    ),
+    learning_rate: float = typer.Option(
+        ..., "-learning_rate", help="Training learning rate."
+    ),
 ):
     print(f"Running experiment {name}")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    epochs = 2
-    batch_size = 1000
-    learning_rate = 0.01
 
     # save the parameters!
 
     # load model
-    model = Net().to(device)
+    model = ser.model.Net().to(device)
 
     # setup params
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
@@ -87,30 +94,6 @@ def train(
                 )
 
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
-        return output
 
 
 @main.command()
