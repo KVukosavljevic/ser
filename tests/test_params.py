a7b6afffd6ef48c034b8a9faebb751a641097c83
CDTAdmin <cdtadmin@ndmpc898.ndm.ox.ac.uk>
2022-10-03 15:47:26 +0100

    Added tests for reading and loading the hyperparameters

---
+++ tests/test_params.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from ser.params import Params, load_params, save_params
+import os
+import pytest
+
+PARAMS_FILE = "model_params.json"
+
+
+@pytest.fixture
+def foo():
+    # setup image
+    # transform it
+    # something else
+    img = 0
+    yield img
+    # run after test
+
+
+def test_save_params():
+    test_path = Path("./tests")
+    test_params = Params("test_exp", 0, 0, 0, "fergfgerg", 0.3, 2)
+
+    save_params(test_path, test_params)
+
+    # test whether file exists
+    assert (test_path / PARAMS_FILE).is_file() is True
+
+    # remove tmpdir
+    os.remove(test_path / PARAMS_FILE)
+
+
+def test_load_params():
+    test_path = Path("./tests")
+    test_params = Params("test_exp", 0, 0, 0, "fergfgerg", 0.3, 2)
+
+    save_params(test_path, test_params)
+    loaded_params = load_params(test_path)
+
+    # test whether file exists
+    assert loaded_params == test_params
+
+    # remove tmpdir
+    os.remove(test_path / PARAMS_FILE)
